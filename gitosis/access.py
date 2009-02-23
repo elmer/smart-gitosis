14d1749bd2bc6085b389a5566a36897074ddebc4
James A. Duncan <james@xen01.gateway.2wire.net>
2009-02-23 18:27:08 -0500

    okay, we need to read from the management console instead

--- gitosis/access.py
+++ gitosis/access.py
@@ -1,7 +1,9 @@
 import os, logging
 from ConfigParser import NoSectionError, NoOptionError
-
 from gitosis import group
+
+import httplib
+import simplejson as json 
 
 def haveAccess(config, user, mode, path):
     """
@@ -12,77 +14,36 @@ def haveAccess(config, user, mode, path):
 
     Returns ``None`` for no access, or a tuple of toplevel directory
     containing repositories and a relative path to the physical repository.
-    """
-    log = logging.getLogger('gitosis.access.haveAccess')
 
-    log.debug(
-        'Access check for %(user)r as %(mode)r on %(path)r...'
-        % dict(
-        user=user,
-        mode=mode,
-        path=path,
-        ))
+    expects to find the following in the gitosis config
+
+    [rsp]
+    haveAccessURL = www.example.org
+    """
+
+    access_url = config.get('rsp', 'haveAccessURL')
+    if not access_url:
+        raise Exception("uh... not configured with an haveAccessURL yet, add it under an [rsp] section")
 
     basename, ext = os.path.splitext(path)
     if ext == '.git':
-        log.debug(
-            'Stripping .git suffix from %(path)r, new value %(basename)r'
-            % dict(
-            path=path,
-            basename=basename,
-            ))
         path = basename
 
-    for groupname in group.getMembership(config=config, user=user):
-        try:
-            repos = config.get('group %s' % groupname, mode)
-        except (NoSectionError, NoOptionError):
-            repos = []
-        else:
-            repos = repos.split()
+    conn = httplib.HTTPConnection(access_url)
+    conn.request("GET", "/hosts/%s/committers/%s" % (path, user))
+    http_response = conn.getresponse()
 
-        mapping = None
+    if http_response.status in range(200,300):
+        # basically I'm lazy and didn't want to handle redirects
+        # if that turns out to be a bad thing I'm sure I can write that
+        # also, if rsp is down I just return false
+        response = json.loads(http_response.read())
+    else:
+        return None
 
-        if path in repos:
-            log.debug(
-                'Access ok for %(user)r as %(mode)r on %(path)r'
-                % dict(
-                user=user,
-                mode=mode,
-                path=path,
-                ))
-            mapping = path
-        else:
-            try:
-                mapping = config.get('group %s' % groupname,
-                                     'map %s %s' % (mode, path))
-            except (NoSectionError, NoOptionError):
-                pass
-            else:
-                log.debug(
-                    'Access ok for %(user)r as %(mode)r on %(path)r=%(mapping)r'
-                    % dict(
-                    user=user,
-                    mode=mode,
-                    path=path,
-                    mapping=mapping,
-                    ))
-
-        if mapping is not None:
-            prefix = None
-            try:
-                prefix = config.get(
-                    'group %s' % groupname, 'repositories')
-            except (NoSectionError, NoOptionError):
-                try:
-                    prefix = config.get('gitosis', 'repositories')
-                except (NoSectionError, NoOptionError):
-                    prefix = 'repositories'
-
-            log.debug(
-                'Using prefix %(prefix)r for %(path)r'
-                % dict(
-                prefix=prefix,
-                path=mapping,
-                ))
-            return (prefix, mapping)
+    if response['canCommit']:
+        prefix = 'repositories'
+        mapping = path
+        return (prefix, mapping)
+    else:
+        return None
