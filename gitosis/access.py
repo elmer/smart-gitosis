01af3f1afea9b3ff111f763acdb48d8217a9c544
Anders Conbere <aconbere@gmail.com>
2009-02-24 10:44:40 -0800

    moved rsp calls into check_with_rsp function, and short curcuited haveAccess with that return from that call

--- gitosis/access.py
+++ gitosis/access.py
@@ -1,9 +1,7 @@
 import os, logging
 from ConfigParser import NoSectionError, NoOptionError
-from gitosis import group
 
-import httplib
-import simplejson as json 
+from gitosis import group
 
 def haveAccess(config, user, mode, path):
     """
@@ -14,13 +12,92 @@ def haveAccess(config, user, mode, path):
 
     Returns ``None`` for no access, or a tuple of toplevel directory
     containing repositories and a relative path to the physical repository.
+    """
+    log = logging.getLogger('gitosis.access.haveAccess')
 
-    expects to find the following in the gitosis config
+    log.debug(
+        'Access check for %(user)r as %(mode)r on %(path)r...'
+        % dict(
+        user=user,
+        mode=mode,
+        path=path,
+        ))
 
+
+    basename, ext = os.path.splitext(path)
+    if ext == '.git':
+        log.debug(
+            'Stripping .git suffix from %(path)r, new value %(basename)r'
+            % dict(
+            path=path,
+            basename=basename,
+            ))
+        path = basename
+
+    response = check_with_rsp(config, user, mode, path)
+    if response:
+        return response
+
+    for groupname in group.getMembership(config=config, user=user):
+        try:
+            repos = config.get('group %s' % groupname, mode)
+        except (NoSectionError, NoOptionError):
+            repos = []
+        else:
+            repos = repos.split()
+
+        mapping = None
+
+        if path in repos:
+            log.debug(
+                'Access ok for %(user)r as %(mode)r on %(path)r'
+                % dict(
+                user=user,
+                mode=mode,
+                path=path,
+                ))
+            mapping = path
+        else:
+            try:
+                mapping = config.get('group %s' % groupname,
+                                     'map %s %s' % (mode, path))
+            except (NoSectionError, NoOptionError):
+                pass
+            else:
+                log.debug(
+                    'Access ok for %(user)r as %(mode)r on %(path)r=%(mapping)r'
+                    % dict(
+                    user=user,
+                    mode=mode,
+                    path=path,
+                    mapping=mapping,
+                    ))
+
+        if mapping is not None:
+            prefix = None
+            try:
+                prefix = config.get(
+                    'group %s' % groupname, 'repositories')
+            except (NoSectionError, NoOptionError):
+                try:
+                    prefix = config.get('gitosis', 'repositories')
+                except (NoSectionError, NoOptionError):
+                    prefix = 'repositories'
+
+            log.debug(
+                'Using prefix %(prefix)r for %(path)r'
+                % dict(
+                prefix=prefix,
+                path=mapping,
+                ))
+            return (prefix, mapping)
+
+def check_with_rsp(config, user, mode, path):
+    """
+    expects to find the following in the gitosis config 
     [rsp]
     haveAccessURL = www.example.org
     """
-
     access_url = config.get('rsp', 'haveAccessURL')
     if not access_url:
         raise Exception("uh... not configured with an haveAccessURL yet, add it under an [rsp] section")
@@ -30,7 +107,7 @@ def haveAccess(config, user, mode, path):
         path = basename
 
     conn = httplib.HTTPConnection(access_url)
-    conn.request("GET", "/hosts/%s/committers/%s" % (path, user))
+    conn.request("GET", "/%s/committers/%s" % (path, user))
     http_response = conn.getresponse()
 
     if http_response.status in range(200,300):
@@ -41,7 +118,7 @@ def haveAccess(config, user, mode, path):
     else:
         return None
 
-    if response['canCommit']:
+    if response['canCommit'] == "1":
         prefix = 'repositories'
         mapping = path
         return (prefix, mapping)
