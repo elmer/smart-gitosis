936237cd10cf575b2535f1e1a45818a9116bba4b
Anders Conbere <aconbere@gmail.com>
2009-04-29 08:40:19 -1000

    adding sending an amqp message to configured host and exchange to the post_update

--- gitosis/run_hook.py
+++ gitosis/run_hook.py
@@ -7,6 +7,7 @@ import logging
 import os
 import sys
 import shutil
+import ConfigParser
 
 from gitosis import repository
 from gitosis import ssh
@@ -14,6 +15,55 @@ from gitosis import gitweb
 from gitosis import gitdaemon
 from gitosis import app
 from gitosis import util
+
+def build_amqp_resources(ch):
+    ch.exchange_declare('gitosis.post_update', 'direct', auto_delete=False,
+                        durable=False)
+    ch.queue_declare(queue="gitosis.post_update", durable=True,
+                     exclusive=False, auto_delete=False)
+
+    ch.queue_bind("gitosis.post_update", 'gitosis.post_update')
+
+def amqp_hook(config):
+    try:
+        use_amqp = config.get("gitosis", "amqp")
+    except (ConfigParser.NoSectionError,
+            ConfigParser.NoOptionError):
+        use_amqp = false
+
+    if use_amqp:
+        try:
+            amqp_host = config.get("gitosis", "amqp_host")
+            amqp_user = config.get("gitosis", "amqp_user")
+            amqp_password = config.get("gitosis", "amqp_password")
+            amqp_ssl = config.get("gitosis", "amqp_ssl")
+            amqp_exchange = config.get("gitosis", "amqp_exchange")
+
+            return send_amqp_message(amqp_host, amqp_user, amqp_password,
+                                     ssl=amqp_ssl, exchange=amqp_exchange)
+
+        except (ConfigParser.NoSectionError,
+                ConfigParser.NoOptionError):
+            return False
+    else:
+        return False
+
+def send_amqp_message(host, user_id, password, ssl=True,
+                      exchange="gitosis.post_update"):
+    import amqplib.client_0_8 as amqp
+    import simplejson as json 
+
+    conn = amqp.Connection(host, userid=userid, password=password, ssl=ssl)
+
+    ch = conn.channel()
+    ch.access_request('/data', active=True, write=True)
+
+    build_amqp_resources(ch)
+    msg = amqp.Message("git_repository_updated", content_type='text/plain')
+    ch.basic_publish(msg, exchange)
+
+    ch.close()
+    conn.close()
 
 def post_update(cfg, git_dir):
     export = os.path.join(git_dir, 'gitosis-export')
@@ -31,6 +81,10 @@ def post_update(cfg, git_dir):
         )
     # re-read config to get up-to-date settings
     cfg.read(os.path.join(export, '..', 'gitosis.conf'))
+    
+    #send a message to the amqp server that there's been an update
+    amqp_hook(config=cfg)
+
     gitweb.set_descriptions(
         config=cfg,
         )
