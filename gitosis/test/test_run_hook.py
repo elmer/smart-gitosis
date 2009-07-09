81cd913f20d8049d47f2bb926481a3477b3fbcba
Anders Conbere <aconbere@gmail.com>
2009-07-09 08:59:02 -1000

    our comand adds the /opt/local/bin path

--- gitosis/test/test_run_hook.py
+++ gitosis/test/test_run_hook.py
@@ -86,6 +86,5 @@ description = blah blah
     got = os.listdir(ssh)
     eq(got, ['authorized_keys'])
     got = readFile(os.path.join(ssh, 'authorized_keys')).splitlines(True)
-    log.debug("GOT: %s" % got)
-    assert 'command="gitosis-serve jdoe",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ssh-somealgo 0123456789ABCDEFBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB= jdoe@host.example.com\n' in got, \
+    assert 'command="PATH=/opt/local/bin gitosis-serve jdoe",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ssh-somealgo 0123456789ABCDEFBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB= jdoe@host.example.com\n' in got, \
         "SSH authorized_keys line for jdoe not found: %r" % got
