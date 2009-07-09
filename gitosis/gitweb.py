896b6eb336cb825deef4990f77abb646edd47363
Anders Conbere <aconbere@gmail.com>
2009-07-08 14:41:59 -1000

    adding __repr__ to repository object

--- gitosis/gitweb.py
+++ gitosis/gitweb.py
@@ -42,6 +42,9 @@ class Repository(object):
             return "%s %s" % (self.name, self.owner)
         else:
             return self.name
+
+    def __repr__(self):
+        return self.__str__()
         
 
 def _escape_filename(s):
