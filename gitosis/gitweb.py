ec754542a3337d698b596161b177ce2db1155e71
Anders Conbere <aconbere@gmail.com>
2009-07-08 14:27:41 -1000

    found a bug with filter_repositories, reworking the loops to rename the repo objects name value

--- gitosis/gitweb.py
+++ gitosis/gitweb.py
@@ -80,13 +80,15 @@ def filter_repositories(repo_dir, config_repos):
     """
     filtered_repos = []
     for repo in config_repos:
-        if path.exists(path.join(repo_dir, repo.name)) or \
-           path.exists(path.join(repo_dir, "%s.git" % repo.name)):
-            filtered_repos.append(repo)
-        else:
-            log.warning(
-                'Cannot find %(name)r in %(repo_dir)r'
-                % dict(name=repo.name, repo_dir=repo_dir))
+        if not path.exist(path.join(repo_dir, repo.name)):
+            repo.name = "%s.git" % repo.name
+            if not path.exists(path.join(repo_dir, repo.name)):
+                log.warning(
+                    'Cannot find %(name)r in %(repo_dir)r'
+                    % dict(name=repo.name, repo_dir=repo_dir))
+                continue
+        filtered_repos.append(repo)
+            
     return filtered_repos
 
 def generate_project_list(config):
